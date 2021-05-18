4b86a66af28e7bba3d1bcab7a87eb4533f7c18cd
Ben Kramer <benkramer614@gmail.com>
2021-05-18 08:18:05 -0400

    test 3 pass

--- index.js
+++ index.js
@@ -4,4 +4,11 @@
 //   return num1 * num2
 const num1 = 2
 const num2 = 31
+const num3 = 8
+const num4 = 2
 const multiply = num1 * num2
+const random = Math.random()*10
+
+const mod = num3 / num4
+
+const max = Math.max(20)
