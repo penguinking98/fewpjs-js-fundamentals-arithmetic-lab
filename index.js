5b74f7772d089085d2b5d54429b0e8e62499adce
Ben Kramer <benkramer614@gmail.com>
2021-05-18 18:21:01 -0400

    commit tree 7a1ccc6b155fb08d96a13bbfe68cdd852eed417a

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const num1 = 2
 const num2 = 31
 const num3 = 8
 const num4 = 2
+const min = 0
+const maxx = 100
 const multiply = num1 * num2
-const random = Math.random()*10
-
+const random = Math.floor(Math.random() * (maxx - min + 1)) + min
 const mod = num3 / num4
 
 const max = Math.max(20)
